1c492bcb89dd26ec7093dea46f7c3eed985b8d63
Mauricio Viana de Moura <50327265+MauricioVianaMoura@users.noreply.github.com>
2025-02-23 21:29:48 -0300

    Adicionado projeto de Aula

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ConsoleApp1.Classes;
+
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Classe Program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Método Main
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Person pessoa = new Person("Mauricio","Viana",29);
+            Console.WriteLine(pessoa.Nome);
+        }
+    }
+}
